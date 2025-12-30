6f4306ec3de7714b4d938db8581df849c1f1d927
Archie P <porovarchita0803@gmail.com>
2025-12-30 21:20:19 +0530

    Email Integration

--- src/main/java/com/taskreminder/app/AppApplication.java
+++ src/main/java/com/taskreminder/app/AppApplication.java
@@ -1,13 +1,22 @@
 package com.taskreminder.app;
 
+import com.taskreminder.app.Service.EmailService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class AppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AppApplication.class, args);
+
+//		ApplicationContext context = (ApplicationContext) SpringApplication.run(AppApplication.class, args);
+//
+//		EmailService emailService = context.getBean(EmailService.class);
+//		emailService.sendEmail("2708archita@gmail.com","Test Mail","Hey, this is a test mail");
+//
 	}
 
 }
---
+++ src/main/java/com/taskreminder/app/Service/EmailService.java
@@ -0,0 +1,58 @@
+package com.taskreminder.app.Service;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Properties;
+
+@Service
+public class EmailService {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    public void sendEmail(String to, String subject, String body) {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", String.valueOf(port));
+        Session session = Session.getInstance(props, getPasswordAuthentication());
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(
+                    Message.RecipientType.TO, InternetAddress.parse(to)
+            );
+            message.setSubject(subject);
+            message.setText(body);
+
+            Transport.send(message);
+
+            System.out.println("Email Sent Successfully!");
+        }catch (MessagingException e) {
+            throw new RuntimeException("Failed to send email.", e);
+        }
+    }
+
+    private Authenticator getPasswordAuthentication() {
+        return new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication(){
+                return new PasswordAuthentication(username,password);
+            }
+        };
+    }
+}
